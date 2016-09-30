577c65ccb5f68d3091de2368554802eb78e51ee0
Ubuntu <ubuntu@ip-172-31-1-199.us-west-2.compute.internal>
2016-09-30 02:11:47 +0000

    config file

---
+++ config.dev.sh
@@ -0,0 +1,10 @@
+export PUBLIC_DNS="ec2-54-68-192-60.us-west-2.compute.amazonaws.com"
+export KAFKA_BID_TOPIC="userbid_stream_topic"
+export KAFKA_PARKING_TOPIC="parking_stream_topic"
+export ZOOKEEPER_DNS="ec2-54-68-192-60.us-west-2.compute.amazonaws.com:2181"
+export KAFKA_BROKERS="ec2-54-68-192-60.us-west-2.compute.amazonaws.com:9092, ec2-52-36-116-225.us-west-2.compute.amazonaws.com:9092, ec2-52-33-125-42.us-west-2.compute.amazonaws.com:9092"
+export ES_DNS="ec2-54-148-46-219.us-west-2.compute.amazonaws.com"
+
+export REDIS_HOST="ec2-52-36-186-92.us-west-2.compute.amazonaws.com"
+export REDIS_PASSWORD="srivats"
+
